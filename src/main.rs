665f0bbd1f4cca3b1a481885342b55e201107d12
Lokathor <zefria@gmail.com>
2022-08-14 15:23:47 -0600

    print the lex of the file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use dmgrs::lexer::DmgToken;
+use logos::Logos;
+
+const SRC: &str = include_str!("../samples/hello_world.s");
+
 fn main() {
-  println!("hello");
+  let mut eol_count = 0;
+  for t in DmgToken::lexer(SRC) {
+    match t {
+      DmgToken::EndOfLine => {
+        if eol_count == 0 {
+          print!("{t:?}");
+          eol_count += 1;
+        } else {
+          eol_count += 1;
+        }
+      }
+      other => {
+        if eol_count != 0 {
+          if eol_count > 1 {
+            print!(" x{eol_count}");
+          }
+          println!();
+          eol_count = 0;
+        }
+        println!("{other:?}");
+      }
+    }
+  }
 }
