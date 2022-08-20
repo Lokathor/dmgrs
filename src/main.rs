6faf53352df4f5a3e51c913096677ac4a36870d9
Lokathor <zefria@gmail.com>
2022-08-19 20:16:37 -0600

    .

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,13 @@ use dmgrs::{easy_lexer::EasyLexer, parser::Ast};
 const SRC: &str = include_str!("../samples/minimum-program.s");
 
 fn main() {
-  println!("EasyLexer: {} lexemes found.", EasyLexer::new(SRC).count());
-  for (lex, _r) in EasyLexer::new(SRC) {
-    print!("{lex:?} ");
-  }
-
   let lexer = EasyLexer::new(SRC);
 
   let stream = Stream::from_iter(SRC.len()..SRC.len(), lexer);
 
-  println!("{:?}", Ast::parser().parse(stream).unwrap());
+  let ast = Ast::parser().parse(stream).unwrap();
+
+  for item in ast.items.iter() {
+    println!("Item: {item:?}");
+  }
 }
