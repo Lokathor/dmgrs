44cccc88849d10082c18eee68f9963d3984ee619
Lokathor <zefria@gmail.com>
2022-08-21 18:31:46 -0600

    we can make the most basic of roms.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,35 @@
-use chumsky::{Parser, Stream};
-use dmgrs::{easy_lexer::EasyLexer, header::Header, parser::Ast};
+use dmgrs::{
+  header::Header,
+  section::{Condition, Section, SectionLine},
+};
+use tinyvec::SliceVec;
 
+#[allow(dead_code)]
 const SRC: &str = include_str!("../samples/minimum-program.s");
 
 fn main() {
-  let lexer = EasyLexer::new(SRC);
+  let mut s_main = Section {
+    lines: vec![
+      SectionLine::Label("main"),
+      SectionLine::Label("main~loop0"),
+      SectionLine::Nop,
+      SectionLine::JumpRelativeLabel("main~loop0", Condition::Always),
+    ],
+  };
+  // If any relative jumps go to far, or point at missing labels, this fails
+  s_main.resolve_relative_label_jumps().unwrap();
 
-  let stream = Stream::from_iter(SRC.len()..SRC.len(), lexer);
-
-  let ast = Ast::parser().parse(stream).unwrap();
-
-  for item in ast.items.iter() {
-    println!("Item: {item:?}");
+  // Generate a blank 32k block of memory
+  let mut v = vec![0_u8; 0x8000];
+  // place the header
+  v[0x100..0x150].copy_from_slice(Header::default().as_bytes());
+  // place the main section code
+  let buf = SliceVec::from_slice_len(&mut v[0..s_main.required_size()], 0);
+  s_main.write_to(buf);
+  // trim blank bytes off the end of the rom
+  while let Some(&0) = v.last() {
+    v.pop();
   }
-
-  println!("Default Header: {:#?}", Header::default());
-
-  let mut v = vec![0_u8; 0x100];
-  v.extend(Header::default().as_bytes());
-  v[0] = 0x18;
-  v[1] = (-2_i8) as u8;
+  // write out the result
   std::fs::write("target/demo.gb", &v).unwrap();
 }
